2afb84bdb95fcdbfb001269098d30e3b4df662f7
Celine <chepceline25@gmail.com>
2023-10-18 14:58:27 +0300

    introduced python and add variable, string and data type

---
+++ AcerPythonClass/string.py
@@ -0,0 +1,23 @@
+text = "Hello World"
+course = "SOFTWARE DEVELOPMENT"
+
+print (text)
+
+# Acessing an element in a string
+
+print (text [0])
+print (text [2])
+
+# Size/Length of string
+print(len(text))
+
+# Modifying a string
+print(text.upper())
+print(course.lower())
+
+# String Concatenation -Joining strings
+first = "Hello"
+second = "There"
+
+print(first + second)
+print(first +" " +second)
